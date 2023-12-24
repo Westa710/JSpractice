5d351415b17d1102a09ccfdd9f7a87d2d2157f16
ko <pleiades1424@gmail.com>
2023-12-24 20:21:38 +0900

    commit

--- function/function.js
+++ function/function.js
@@ -1,17 +1,49 @@
 "use strict";
 
-function isLessThan140(text) {
-  return text.length <= 140;
-}
+// function isLessThan140(text) {
+//   return text.length <= 140;
+// }
 
-function alertTweetable(text) {
-  if (text.length <= 5) {
-    alert("you can tweet!");
-  } else {
-    alert("muri");
-  }
-}
+// const isTweetable = function (text) {
+//   return text.length <= 140;
+// };
 
-console.log(isLessThan140("fooo"));
+// function unfollow() {
+//   console.log("フォローを外しました");
+// }
 
-alertTweetable("fooo000");
+// const cancelTweet = function () {
+//   console.log("ツイートをキャンセルしました");
+// };
+
+// function confirmed(fn) {
+//   if (window.confirm("実行しますか？")) {
+//     fn();
+//   }
+// }
+
+// confirmed(cancelTweet);
+
+// function confirmed(fn) {
+//   const input = window.prompt("実行しますか？");
+//   if (input === "実行") {
+//     fn();
+//   }
+// }
+
+// const deleteRepository = function (name) {
+//   window.alert(name + "を削除");
+// };
+
+// confirmed(() => deleteRepository("portfolio"));
+
+// function confirmed(fn) {
+//   const input = window.prompt("実行しますか？");
+//   fn(input);
+// }
+
+// confirmed(function (input2) {
+//   if (input2 === "実行") {
+//     window.alert("リポジトリを削除しました");
+//   }
+// });
