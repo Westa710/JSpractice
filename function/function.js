59a5b23d84037e226404d1490763276229010665
ko <pleiades1424@gmail.com>
2023-12-24 11:19:05 +0900

    commit

---
+++ function/function.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function isLessThan140(text) {
+  return text.length <= 140;
+}
+
+function alertTweetable(text) {
+  if (text.length <= 5) {
+    alert("you can tweet!");
+  } else {
+    alert("muri");
+  }
+}
+
+console.log(isLessThan140("foo"));
+
+alertTweetable("fooo000");
