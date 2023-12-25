7fafe7e19343e1cd18081b478ccd109b4433be52
ko <pleiades1424@gmail.com>
2023-12-25 10:32:17 +0900

    commit

---
+++ API/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// DOM
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+// method
+async function getUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  return users;
+}
+
+async function listUsers() {
+  const users = await getUsers();
+
+  users.forEach(addList);
+}
+
+function addList(user) {
+  const list = document.createElement("li");
+  list.innerText = user.name;
+  lists.appendChild(list);
+}
+
+// event
+window.addEventListener("load", listUsers);
+
+button.addEventListener("click", listUsers);
