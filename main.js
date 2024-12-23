fe1774d640e44debcb3602249ed29b111d07f3aa
ko <pleiades1424@gmail.com>
2024-12-23 22:25:51 +0900

    feat: 分割代入

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// function sum(arr){
+//     return arr.reduce((sum, e) => {
+//         console.log(`sum:${sum + e}`);
+//         return sum + e;
+//     }, 0)
+// }
+
+// const calcNum = (func, numArr) => {
+//     console.log(numArr)
+//     return func(numArr);
+// }
+
+// console.log(calcNum(sum, [1, 2, 3, 4, 5]));
+
+// const myProfile = {
+//     name: 'Westa',
+//     age: 200
+// }
+
+// const message1 = `私の名前は${myProfile.name}です．年齢は${myProfile.age}歳です．`
+// console.log(message1)
+
+// const { name, age } = myProfile
+// const message2 = `私の名前は${name}です．年齢は${age}歳です．`
+// console.log(message2)
+
+// const myProfile = ["Westa", 200];
+
+// const [name, age] = myProfile;
+
+// const message3 = `私の名前は${name}です．年齢は${age}歳です．`
+// console.log(message3)
